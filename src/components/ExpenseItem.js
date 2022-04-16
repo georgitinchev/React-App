92345084bd3065b408da1b0a579b120bda5d47fe
Георги Тинчев <georgi.tinchev.124@gmail.com>
2022-04-16 09:44:08 +0300

    Added import to App.js and removed input from the component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,10 +3,10 @@ import "./ExpenseItem.css";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
